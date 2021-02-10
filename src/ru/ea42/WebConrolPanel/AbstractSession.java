339501d14367872fbf0f9d4c35b6a85dc053ad47
evtyushkin <main@ea42.ru>
2021-02-10 14:48:33 +0300

    add class: Ui_Abstract

--- src/ru/ea42/WebConrolPanel/AbstractSession.java
+++ src/ru/ea42/WebConrolPanel/AbstractSession.java
@@ -1,7 +1,13 @@
 package ru.ea42.WebConrolPanel;
 
-import java.net.Socket;
-
 public abstract class AbstractSession {
     public String id;
+
+    public String GetNextId() {
+        return null;
+    }
+
+    public void addForm(Ui_Abstract ui_abstract) {
+
+    }
 }
